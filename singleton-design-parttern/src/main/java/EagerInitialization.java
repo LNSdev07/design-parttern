ba65c00a2b4164a893652dddfcda8ecf299816e3
Lại Ngọc Sơn <107608752+LNSdev07@users.noreply.github.com>
2023-04-06 23:37:51 +0700

    commit code singleton-parttern
    commit code singleton-parttern

---
+++ singleton-design-parttern/src/main/java/EagerInitialization.java
@@ -0,0 +1,23 @@
+
+// cách tạo thứ nhất
+public class EagerInitialization {
+    private static final EagerInitialization instance = new EagerInitialization();
+
+    private EagerInitialization(){
+
+    }
+
+    public static EagerInitialization getInstance(){
+        return instance;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
